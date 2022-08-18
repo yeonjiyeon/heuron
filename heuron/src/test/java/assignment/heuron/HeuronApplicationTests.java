fcb7243cca5d32e027a91ed9d400d9961aae3f92
yeonjiyeon <dalnim95@naver.com>
2022-08-18 20:57:01 +0900

    init project

---
+++ heuron/src/test/java/assignment/heuron/HeuronApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.heuron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeuronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
