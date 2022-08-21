52b0f187141dbb2b79bb075bb960aa80873e7336
yeonjiyeon <dalnim95@naver.com>
2022-08-21 14:16:11 +0900

    add repository test

---
+++ src/test/java/assignment/heuron/repository/PatientRepositoryTest.java
@@ -0,0 +1,14 @@
+package assignment.heuron.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class PatientRepositoryTest {
+
+
+
+}
