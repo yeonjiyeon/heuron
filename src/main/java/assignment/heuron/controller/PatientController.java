302e3ebe2d108eb42036b6808807ebdaa369a99c
yeonjiyeon <dalnim95@naver.com>
2022-08-22 17:16:24 +0900

    feature : 상세조회 api contoller 부분 추가

--- src/main/java/assignment/heuron/controller/PatientController.java
+++ src/main/java/assignment/heuron/controller/PatientController.java
@@ -3,11 +3,14 @@ package assignment.heuron.controller;
 
 import assignment.heuron.domain.Patient;
 import assignment.heuron.dto.request.PatientRequest;
+import assignment.heuron.dto.response.PatientResponse;
 import assignment.heuron.service.PatientService;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +22,18 @@ public class PatientController {
   private final PatientService patientService;
 
   @PostMapping("/patient")
-  public ResponseEntity<Long> savePatient(PatientRequest patientRequest)
+  public ResponseEntity<Long> savePatient(@RequestBody PatientRequest patientRequest)
       throws IOException {
     Long savePatientID = patientService.savePatient(patientRequest);
 
     return new ResponseEntity<>(savePatientID, HttpStatus.OK);
   }
+
+  @GetMapping("/patient/{id}")
+  public ResponseEntity<PatientResponse> readDetailPatient(@PathVariable Long id){
+    PatientResponse patientResponse = patientService.readDetailPatient(id);
+    return new ResponseEntity<>(patientResponse, HttpStatus.OK);
+  }
+
 
 }
