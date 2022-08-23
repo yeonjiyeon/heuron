05a53d9c1dd254941f580e683b66c68aee634f0f
yeonjiyeon <dalnim95@naver.com>
2022-08-23 17:58:00 +0900

    test : 저장 repository test

--- src/test/java/assignment/heuron/repository/PatientRepositoryTest.java
+++ src/test/java/assignment/heuron/repository/PatientRepositoryTest.java
@@ -4,21 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import assignment.heuron.domain.Patient;
 import assignment.heuron.domain.PatientGender;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 class PatientRepositoryTest {
+
+  @Autowired
+  PatientRepository patientRepository;
+
   @Nested
   @DisplayName("환자 정보 저장")
-  class SavePatient{
+  class SavePatient {
+
     @Test
     @DisplayName("환자 정보가 모두 있는 경우")
-    public void patientDataAllExist(){
+    public void patientDataAllExist() {
       //given
       Patient patient = Patient.builder()
           .patientName("patientZ")
@@ -28,16 +35,15 @@ class PatientRepositoryTest {
           .build();
 
       //when
+      Patient savePatient = patientRepository.save(patient);
 
       //then
-
-
-
+      Assertions.assertEquals("patientZ", savePatient.getPatientName());
     }
 
     @Test
     @DisplayName("환자 정보가 일부만 있는 경우")
-    public void patientDataPartExist(){
+    public void patientDataPartExist() {
       //given
       Patient patient = Patient.builder()
           .patientName("patientZ")
@@ -45,49 +51,57 @@ class PatientRepositoryTest {
           .build();
 
       //when
+      patientRepository.save(patient);
 
       //then
     }
 
     @Test
     @DisplayName("사진 정보가 없는 경우")
-    public void imageNotExist(){
+    public void imageNotExist() {
 
-      
     }
 
     @Test
     @DisplayName("사진 정보만 있는 경우")
-    public void onlyImageExist(){}
+    public void onlyImageExist() {
+    }
 
     @Test
     @DisplayName("환자 정보가 모두 없는 경우")
-    public void patientDataNotExist(){}
+    public void patientDataNotExist() {
+    }
 
   }
 
   @Nested
   @DisplayName("환자 정보 조회")
-  class ReadPatient{
+  class ReadPatient {
+
     @Test
     @DisplayName("환자 정보가 모두 있는 경우")
-    public void patientDataAllExist(){}
+    public void patientDataAllExist() {
+    }
 
     @Test
     @DisplayName("환자 정보가 일부만 있는 경우")
-    public void patientDataPartExist(){}
+    public void patientDataPartExist() {
+    }
 
     @Test
     @DisplayName("사진 정보가 없는 경우")
-    public void imageNotExist(){}
+    public void imageNotExist() {
+    }
 
     @Test
     @DisplayName("사진 정보만 있는 경우")
-    public void onlyImageExist(){}
+    public void onlyImageExist() {
+    }
 
     @Test
     @DisplayName("환자 정보가 모두 없는 경우")
-    public void patientDataNotExist(){}
+    public void patientDataNotExist() {
+    }
 
   }
 
