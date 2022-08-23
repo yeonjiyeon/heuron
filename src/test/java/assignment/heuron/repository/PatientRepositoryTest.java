3f9df1fae2acaae58a563e4ef33feae4c15083c3
yeonjiyeon <dalnim95@naver.com>
2022-08-23 18:32:07 +0900

    test : repositoryTest

--- src/test/java/assignment/heuron/repository/PatientRepositoryTest.java
+++ src/test/java/assignment/heuron/repository/PatientRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import assignment.heuron.domain.Patient;
 import assignment.heuron.domain.PatientGender;
+import assignment.heuron.domain.SaveLevel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -51,20 +52,37 @@ class PatientRepositoryTest {
           .build();
 
       //when
-      patientRepository.save(patient);
+      Patient savePatient = patientRepository.save(patient);
 
       //then
+      Assertions.assertEquals(null, savePatient.getPatientGender());
+
+      //null값으로 들어감... validation 적용 필요?
+
     }
 
     @Test
     @DisplayName("사진 정보가 없는 경우")
     public void imageNotExist() {
+      //given
+      Patient patient = Patient.builder()
+          .patientName("patientZ")
+          .patientAge(38)
+          .patientGender(PatientGender.WOMAN)
+          .hasDisease(true)
+          .build();
 
+      //when
+      Patient savePatient = patientRepository.save(patient);
+
+      //then
+      Assertions.assertEquals(SaveLevel.LEVEL1, savePatient.getSaveLevel());
     }
 
     @Test
     @DisplayName("사진 정보만 있는 경우")
     public void onlyImageExist() {
+      //postman으로 확인했을 때는 Bad Request가 뜬다, 하지만 영속성 계층에서 직접 저장하면 저장이 된다.
     }
 
     @Test
